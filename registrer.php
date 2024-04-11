3f6311b5f46e93c429005ace6b422bb3d321d5e3
niyogitangazayvette <159523522+niyogitangazayvette@users.noreply.github.com>
2024-04-11 17:56:08 +0200

    Add files via upload

--- registrer.php
+++ registrer.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "admin";
-$password = "bityear2@2024";
+$username = "root";
+$password = "";
 $dbname = "bityeartwo2024";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
